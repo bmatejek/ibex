7081b90f9570ab89fbf874d283d75fb4655dc5e3
Brian Matejek <bmatejek@gmail.com>
2017-04-04 15:27:26 -0400

    data_structures: added UnionFind data structure

--- data_structures/swc.py
+++ data_structures/swc.py
@@ -1,5 +1,7 @@
+import numpy as np
+
 class SWCEntry:
-    def __init__(self, sample_number, structure_id, x, y, z, radius, parent_sample):
+    def __init__(self, sample_number, structure_id, x, y, z, radius, parent_sample, label):
         self.sample_number = sample_number
         self.structure_id = structure_id
         self.x = x
@@ -7,9 +9,39 @@ class SWCEntry:
         self.z = z
         self.radius = radius
         self.parent_sample = parent_sample
+        self.label = label
 
-    def Point(self):
-        return (self.x, self.y, self.z)
+    def GridPoint(self):
+        grid_location = (self.z, self.y, self.x)
+        return np.array(grid_location).astype(dtype=np.uint32)
+
+    def SampleNumber(self):
+        return self.sample_number
+
+    def StructureID(self):
+        return self.structure_id
+
+    def X(self):
+        return self.x
+
+    def Y(self):
+        return self.y
+
+    def Z(self):
+        return self.z
+
+    def Radius(self):
+        return self.radius
+
+    def ParentSample(self):
+        return self.parent_sample
+
+    def Label(self):
+        return self.label
+
+    def WorldPoint(self, world_res):
+        world_location = (self.z * world_res[0], self.y * world_res[1], self.x * world_res[2])
+        return np.array(world_location).astype(dtype=np.uint32)
 
 class Skeleton:
     def __init__(self, prefix, label):
@@ -17,8 +49,10 @@ class Skeleton:
         self.joints = []
         self.endpoints = []
 
+        # get the skeleton filename
         filename = 'skeletons/' + prefix + '/tree_' + str(label) + '.swc'
 
+        # open the file and read all of the swc entries
         with open(filename, 'r') as fd:
             for joint in fd.readlines():
                 # ignore comments
@@ -36,7 +70,7 @@ class Skeleton:
                 radius = float(attributes[5])
                 parent_sample = int(attributes[6])
 
-                self.joints.append(SWCEntry(sample_number, structure_id, x, y, z, radius, parent_sample))
+                self.joints.append(SWCEntry(sample_number, structure_id, x, y, z, radius, parent_sample, label))
 
         # go through every joint to determine endpoints
         endpoints = [True] * len(self.joints)
@@ -51,6 +85,37 @@ class Skeleton:
             if joint.parent_sample == -1:
                 endpoints[ie] = True
 
+        # create a list of endpoints 
         for ie in range(len(endpoints)):
             if endpoints[ie]:
                 self.endpoints.append(self.joints[ie])
+
+    # return the kth endpoint
+    def EndPoint(self, k):
+        assert (0 <= k and k < self.NEndPoints())
+        return self.endpoints[k]
+
+    # return all endpoints
+    def EndPoints(self):
+        return self.endpoints
+
+    # return the number of endpoints
+    def NEndPoints(self):
+        return len(self.endpoints)
+
+    # return the kth joint
+    def Joint(self, k):
+        assert (0 <= k and k < self.NJoints())
+        return self.joints[k]
+
+    # return all joints
+    def Joints(self):
+        return self.joints
+
+    # return the number of joints
+    def NJoints(self):
+        return len(self.joints)
+
+    # return the segment label
+    def Label(self):
+        return self.label
