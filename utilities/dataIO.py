135f517986bd96aee610f6bf909c19cfc0706c83
Brian Matejek <bmatejek@gmail.com>
2017-04-04 15:31:03 -0400

    utilities: added code to read segmentation and gold files

--- utilities/dataIO.py
+++ utilities/dataIO.py
@@ -1,19 +1,70 @@
+import os
 import h5py
 import numpy as np
+from ibex.data_structures import swc
 
-def ReadMeta(prefix):
+def ReadMetaData(prefix):
+    # generate the meta data default filename
     filename = 'meta_data/' + prefix + '.meta'
 
+    # open the meta data filename
     with open(filename, 'r') as fd:
         meta_data = fd.readlines()
-
         resolutions = meta_data[1].split('x')
 
-        return (int(resolutions[2]), int(resolutions[1]), int(resolutions[0]))
+    # return the resolution in nanometers (z, y, x)
+    return (int(resolutions[2]), int(resolutions[1]), int(resolutions[0]))
+
+
 
 def ReadH5File(filename, dataset):
+    # read the h5py file
     with h5py.File(filename, 'r') as hf:
         data = np.array(hf[dataset])
 
+    # return the data
     return data
-        
+
+
+
+def ReadSegmentationData(prefix):
+    filename = 'rhoana/{0}_rhoana.h5'.format(prefix)
+
+    return ReadH5File(filename, 'main')
+
+
+
+def ReadGoldData(prefix):
+    filename = 'gold/{0}_gold.h5'.format(prefix)
+
+    return ReadH5File(filename, 'stack')
+
+
+
+def ReadSkeletons(prefix, data):
+    # read in all of the skeletons
+    skeletons = []
+    joints = []
+    endpoints = []
+
+    for label in np.unique(data):
+        skeleton_filename = 'skeletons/{}/tree_{}.swc'.format(prefix, label)
+
+        # see if the skeleton exists
+        if not os.path.isfile(skeleton_filename):
+            continue
+
+        # read the skeleton
+        skeleton = swc.Skeleton(prefix, label)
+
+        skeletons.append(skeleton)
+
+        # add all joints for this skeleton
+        for ij in range(skeleton.NJoints()):
+            joints.append(skeleton.Joint(ij))
+        # add all endpoints for this skeleton
+        for ip in range(skeleton.NEndPoints()):
+            endpoints.append(skeleton.EndPoint(ip))
+
+    # return all of the skeletons
+    return skeletons, joints, endpoints
