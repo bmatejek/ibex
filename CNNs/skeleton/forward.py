c5e8d348f32cf3919281ef2582cb3ad5c51ad91f
Brian Matejek <bmatejek@gmail.com>
2017-04-25 16:46:41 -0400

    forward.py: removed debugging statement

--- CNNs/skeleton/forward.py
+++ CNNs/skeleton/forward.py
@@ -87,7 +87,6 @@ def Forward(prefix, maximum_distance, model_prefix, window_width=106, nchannels=
     # get the candidate locations 
     # there is no reason to use the padded data - that is only for training
     candidates = FindCandidates(prefix, maximum_distance, padding=0, forward=True)
-    candidates = candidates[:100]
     ncandidates = len(candidates)
 
     # create an array of labels
