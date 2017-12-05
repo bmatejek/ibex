7e36951aeacae70e0c9162ae2bd21ea6a6fbd95a
bmatejek <bmatejek@gmail.com>
2017-12-05 16:34:48 -0500

    super resolution for one level complete

--- super_resolution/single_image_sr.py
+++ super_resolution/single_image_sr.py
@@ -11,7 +11,9 @@ from ibex.utilities import dataIO
 
 
 # small utility functions
+@jit(nopython=True)
 def ceil(value): return int(math.ceil(value))
+@jit(nopython=True)
 def floor(value): return int(math.floor(value))
 
 def Gaussian(diameter, sigma):
@@ -180,89 +182,73 @@ class Constraint:
         self.sigma = sigma
 
 # get the next level 
-def Upsample(grayscales, kdtrees, level, parameters):
-    # get useful parameters
-    m = parameters['m']
+def Upsample(grayscales, kdtrees, n, parameters):
+    # get useful parameters and variables
     scale = parameters['scale']
     diameter = parameters['diameter']
     radius = (diameter[0] / 2, diameter[1] / 2)
 
-    # get the original input size
-    yres, xres = grayscales[level - 1].shape
-    scale_factor = pow(scale, level)
-    high_yres, high_xres = (floor(yres * scale_factor), floor(xres * scale_factor))
+    # create an empty high res image
+    yres, xres = grayscales[0].shape
+    magnification = pow(scale, n)
+    high_yres, high_xres = (ceil(yres * magnification), ceil(xres * magnification))
+    grayscales[n] = np.zeros((high_yres, high_xres), dtype=np.float32)
 
-    # create an empty grayscale level
-    grayscales[level] = np.zeros((high_yres, high_xres), dtype=np.float32)
 
-    constraints = []
+    # consider all high res images from 0 to level
+    for ii in range(n):
+        # get this image
+        image = grayscales[ii]
 
-    # iterate through all of the high res images
-    for iv in range(level):
-        input_yres, input_xres = grayscales[iv].shape
-        l = iv - level
+        # get the size of this image
+        yres, xres = image.shape
 
-        # iterate over all pixels in this image
-        for iy in range(radius[0], input_yres - radius[0]):
+        # get the difference between n and this level
+        delta = n - ii
+
+        # get the low resolution image
+        lowres_image_shape = grayscales[-1 * delta].shape
+        lowres_kdtree = kdtrees[-1 * delta]
+
+        # get the scale from the lowres image to image
+        magnification = pow(scale, delta)
+
+        # iterate through all valid locations
+        for iy in range(radius[0], yres - radius[0]):
             print iy
-            import sys
-            sys.stdout.flush()
-            for ix in range(radius[1], input_xres - radius[1]):
-                # get the feature at this level
-                feature = grayscales[iv][iy-radius[0]:iy+radius[0]+1,ix-radius[1]:ix+radius[1]+1].flatten()
+            for ix in range(radius[1], xres - radius[1]):
+                # get the feature at this location
+                feature = image[iy-radius[0]:iy+radius[0]+1,ix-radius[0]:ix+radius[1]+1].flatten()
 
-                # look in the corresponding low resolution image for nearby features
-                value, location = kdtrees[l].query(feature, 1)
-                lowy, lowx = IndexToIndices(location, grayscales[l].shape, diameter)
+                # search for the closest feature in the lowres_image
+                _, location = lowres_kdtree.query(feature, 1)
 
-                # get the location at this level
-                thisy = int(round(lowy * pow(scale, -1 * l)))
-                thisx = int(round(lowx * pow(scale, -1 * l)))
-                outputx = floor(diameter[0] * pow(scale, -1 * l) / 2)
-                outputy = floor(diameter[1] * pow(scale, -1 * l) / 2)
+                lowres_y, lowres_x = IndexToIndices(location, lowres_image_shape, diameter)
+                high_radius = (floor(diameter[0] * magnification / 2), floor(diameter[1] * magnification / 2))
+                highy, highx = (int(round(lowres_y * magnification)), int(round(lowres_x * magnification)))
 
-                highy = iy * pow(scale, -1 * l)
-                highx = ix * pow(scale, -1 * l)
-                sigma = math.sqrt(pow(scale, -1 * l))
+                # scale iy and ix
+                scaled_iy = int(round(magnification * iy))
+                scaled_ix = int(round(magnification * ix))
 
-                xs = np.arange(thisx-outputx, thisx+outputx+1)
-                ys = np.arange(thisy-outputy, thisy+outputy+1)
-
-                constraints.append(Constraint(highy, highx, ys, xs, grayscales[iv][iy,ix], sigma))
-
-    # solve the system of constraints
-    nconstraints = len(constraints)
-    H = np.zeros((nconstraints, high_yres * high_xres), dtype=np.float32)
-    b = np.zeros(nconstraints, dtype=np.float32)
-
-    # populate H for every constraint
-    for ie, constraint in enumerate(constraints):
-        ys = constraint.ys
-        xs = constraint.xs
-        sigma = constraint.sigma
-
-        # get this gaussian kernel
-        gaussian_kernel = Gaussian((xs.size, ys.size), sigma)
-
-        for iv, iy in enumerate(ys):
-            for iu, ix in enumerate(xs):
-                gaussian_value = gaussian_kernel[iv,iu]
-
-                index = IndicesToIndex(ix, iy, (high_yres, high_xres))
-                H[ie, index] = gaussian_value 
-
-        # set the value
-        b[ie] = constraint.value
+                # get the corresponding image section
+                grayscales[n][scaled_iy-high_radius[0]:scaled_iy+high_radius[0]+1,scaled_ix-high_radius[1]:scaled_ix+high_radius[1]+1] = image[highy-high_radius[0]:highy+high_radius[0]+1,highx-high_radius[1]:highx+high_radius[1]+1]
 
 
-    print H.shape
-    highres, _ = np.linalg.lstsq(H, b)
-    print highres
 
-    for iy in range(yres):
-        for ix in range(xres):
-            index = IndicesToIndex(ix, iy, (high_yres, high_xres))
-            grayscales[level][iy,ix] = highres[index]
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -277,7 +263,6 @@ def SingleImageSR(root_filename, parameters):
 
     # read the image into a float32 array between [0, 1]
     image = dataIO.ReadImage('{}.png'.format(root_filename))
-    image = image[0:40,:,:]
     yres, xres, depth = image.shape
     assert (depth == 3)
 
@@ -290,7 +275,12 @@ def SingleImageSR(root_filename, parameters):
     # get the distance for all images
     distances = DistanceThreshold(grayscales, shifts, parameters)
 
+    #FeatureMatching(root_filename, grayscales, kdtrees, parameters)
+
+
     Upsample(grayscales, kdtrees, 1, parameters)
+
+    dataIO.WriteImage('output.png', grayscales[1])
 
 
     # # create all of the images from 1 to n
@@ -298,7 +288,7 @@ def SingleImageSR(root_filename, parameters):
     #     Upsample(grayscales, kdtrees, iv, parameters)
 
 
-    dataIO.WriteImage('output.jpg', grayscales[1])
+    #dataIO.WriteImage('output.jpg', grayscales[1])
 
     # for iv in range(-1, -(m + 1), -1):
     #     grayscale_filename = '{}{}.png'.format(root_filename, iv)
@@ -310,20 +300,54 @@ def SingleImageSR(root_filename, parameters):
     # import sys
     # sys.exit()
 
+#################################
+#### VISUALIZATION FUNCTIONS ####
+#################################
 
+def BoundingBox(output_filename, image, ix, iy, diameter):
+    radius = (diameter[0] / 2, diameter[1] / 2)
 
+    # get the high and low parameters for the image
+    xlow = (ix - radius[1] - 1)
+    xhigh = (ix + radius[1] + 1)
+    ylow = (iy - radius[0] - 1)
+    yhigh = (iy + radius[0] + 1)
 
+    image[ylow:yhigh+1,xlow] = 1
+    image[ylow:yhigh+1,xhigh] = 1
+    image[ylow,xlow:xhigh+1] = 1
+    image[yhigh,xlow:xhigh+1] = 1
 
+    dataIO.WriteImage(output_filename, image)
 
+def FeatureMatching(root_filename, grayscales, kdtrees, parameters):
+    # get useful parameters and variables
+    diameter = parameters['diameter']
+    scale = parameters['scale']
 
+    yres, xres = grayscales[0].shape  
+    radius = (diameter[0] / 2, diameter[1] / 2)
 
+    ix = 80
+    iy = 80
 
+    # get this feature
+    feature = grayscales[0][iy-radius[0]:iy+radius[0]+1,ix-radius[1]:ix+radius[1]+1].flatten()
 
+    # find the closest feature in the next lowest level
+    value, location = kdtrees[-1].query(feature, 1)
 
+    # get the coordinate in this lower level
+    lowy, lowx = IndexToIndices(location, grayscales[-1].shape, diameter)
 
+    BoundingBox('{}-bounding.png'.format(root_filename), grayscales[0], ix, iy, diameter)
+    BoundingBox('{}-bounding-low.png'.format(root_filename), grayscales[-1], lowx, lowy, diameter)
 
-
-
+    # get the difference between the scales
+    increase = pow(scale, 1)
+    high_diameter = (floor(diameter[0] * increase), floor(diameter[1] * increase))
+    highy, highx = (int(round(lowy * increase)), int(round(lowx * increase)))
+    BoundingBox('{}-bounding-high.png'.format(root_filename), grayscales[0], highx, highy, high_diameter)
 
 
 
@@ -432,3 +456,89 @@ def BicubicInterpolation(input_filename, scale):
 
     output_filename = '{}-bicubic-interpolation.png'.format(input_filename.split('.')[0])
     dataIO.WriteImage(output_filename, output_image)
+
+
+
+
+# # get useful parameters
+#     m = parameters['m']
+#     scale = parameters['scale']
+#     diameter = parameters['diameter']
+#     radius = (diameter[0] / 2, diameter[1] / 2)
+
+#     # get the original input size
+#     yres, xres = grayscales[level - 1].shape
+#     scale_factor = pow(scale, level)
+#     high_yres, high_xres = (floor(yres * scale_factor), floor(xres * scale_factor))
+
+#     # create an empty grayscale level
+#     grayscales[level] = np.zeros((high_yres, high_xres), dtype=np.float32)
+
+#     constraints = []
+
+#     # iterate through all of the high res images
+#     for iv in range(level):
+#         input_yres, input_xres = grayscales[iv].shape
+#         l = iv - level
+
+#         # iterate over all pixels in this image
+#         for iy in range(radius[0], input_yres - radius[0]):
+#             print iy
+#             import sys
+#             sys.stdout.flush()
+#             for ix in range(radius[1], input_xres - radius[1]):
+#                 # get the feature at this level
+#                 feature = grayscales[iv][iy-radius[0]:iy+radius[0]+1,ix-radius[1]:ix+radius[1]+1].flatten()
+
+#                 # look in the corresponding low resolution image for nearby features
+#                 value, location = kdtrees[l].query(feature, 1)
+#                 lowy, lowx = IndexToIndices(location, grayscales[l].shape, diameter)
+
+#                 # get the location at this level
+#                 thisy = int(round(lowy * pow(scale, -1 * l)))
+#                 thisx = int(round(lowx * pow(scale, -1 * l)))
+#                 outputx = floor(diameter[0] * pow(scale, -1 * l) / 2)
+#                 outputy = floor(diameter[1] * pow(scale, -1 * l) / 2)
+
+#                 highy = iy * pow(scale, -1 * l)
+#                 highx = ix * pow(scale, -1 * l)
+#                 sigma = math.sqrt(pow(scale, -1 * l))
+
+#                 xs = np.arange(thisx-outputx, thisx+outputx+1)
+#                 ys = np.arange(thisy-outputy, thisy+outputy+1)
+
+#                 constraints.append(Constraint(highy, highx, ys, xs, grayscales[iv][iy,ix], sigma))
+
+#     # solve the system of constraints
+#     nconstraints = len(constraints)
+#     H = np.zeros((nconstraints, high_yres * high_xres), dtype=np.float32)
+#     b = np.zeros(nconstraints, dtype=np.float32)
+
+#     # populate H for every constraint
+#     for ie, constraint in enumerate(constraints):
+#         ys = constraint.ys
+#         xs = constraint.xs
+#         sigma = constraint.sigma
+
+#         # get this gaussian kernel
+#         gaussian_kernel = Gaussian((xs.size, ys.size), sigma)
+
+#         for iv, iy in enumerate(ys):
+#             for iu, ix in enumerate(xs):
+#                 gaussian_value = gaussian_kernel[iv,iu]
+
+#                 index = IndicesToIndex(ix, iy, (high_yres, high_xres))
+#                 H[ie, index] = gaussian_value 
+
+#         # set the value
+#         b[ie] = constraint.value
+
+
+#     print H.shape
+#     highres, _ = np.linalg.lstsq(H, b)
+#     print highres
+
+#     for iy in range(yres):
+#         for ix in range(xres):
+#             index = IndicesToIndex(ix, iy, (high_yres, high_xres))
+#             grayscales[level][iy,ix] = highres[index]
