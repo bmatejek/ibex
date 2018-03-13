aafa4991e633c1c23d86db7f5c59dead04997888
Brian Matejek <bmatejek@gmail.com>
2018-03-12 22:05:55 -0400

    oracle: updated with new util functions

--- cnns/skeleton/oracle.py
+++ cnns/skeleton/oracle.py
@@ -9,10 +9,10 @@ from ibex.evaluation import comparestacks
 
 
 
-def Oracle(prefix, threshold, maximum_distance, network_distance):
+def Oracle(prefix, threshold, maximum_distance, endpoint_distance, network_distance, filtersize=0):
     # get all of the candidates
-    candidates = FindCandidates(prefix, threshold, maximum_distance, network_distance, inference=True)  
-    ncandidates = len(candidates)
+    positive_candidates = FindCandidates(prefix, threshold, maximum_distance, endpoint_distance, network_distance, 'positive')  
+    ncandidates = len(positive_candidates)
 
     # read in all relevant information
     segmentation = dataIO.ReadSegmentationData(prefix)
@@ -24,9 +24,7 @@ def Oracle(prefix, threshold, maximum_distance, network_distance):
     union_find = [unionfind.UnionFindElement(iv) for iv in range(max_value)]
 
     # iterate over all candidates and collapse edges
-    for candidate in candidates:
-        if not candidate.ground_truth: continue
-
+    for candidate in positive_candidates:
         label_one = candidate.labels[0]
         label_two = candidate.labels[1]
 
@@ -37,11 +35,4 @@ def Oracle(prefix, threshold, maximum_distance, network_distance):
     for iv in range(max_value):
         mapping[iv] = unionfind.Find(union_find[iv]).label
 
-    # output the mapping
-    with open('tmp.txt', 'w') as fd:
-        fd.write('{}\n'.format(max_value))
-        for iv in range(max_value):
-            fd.write('{}\n'.format(mapping[iv]))
-
-
-    #comparestacks.Evaluate(segmentation, gold, filtersize=threshold, anisotropic=anisotropy)
+    comparestacks.CremiEvaluate(segmentation, gold, dilate_ground_truth=1, mask_ground_truth=True, mask_segmentation=False, filtersize=filtersize)
