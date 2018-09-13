40f6f21300b3e90a2e8d72d6b91deeb49c3ef11c
bmatejek <bmatejek@gmail.com>
2018-09-13 16:36:10 -0400

    ebro: removed old code for defunct project

--- cnns/ebro/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-os.environ['THEANO_FLAGS'] = 'device=gpu,floatX=float32'
-
-# import files from this directory
-from train import Train
-from forward import Forward
-from generate_features import GenerateFeatures
-from baseline import Baseline, Ensemble
