a2118e8b127bd8bc6a3ceb4ff70c483e1f9157b5
bmatejek <bmatejek@gmail.com>
2018-09-21 15:48:10 -0400

    seg2gold: fixed bug in c++ array declaration

--- transforms/seg2gold.pyx
+++ transforms/seg2gold.pyx
@@ -11,7 +11,7 @@ def Mapping(segmentation, gold, match_threshold=0.80, nonzero_threshold=0.40):
     if not segmentation.dtype == np.int64: segmentation = segmentation.astype(np.int64)
     if not gold.dtype == np.int64: gold = gold.astype(np.int64)
 
-    cdef np.ndarray[long, ndim=3, mode='c'] cpp_segmentation cpp_segmentation = np.ascontiguousarray(segmentation, dtype=ctypes.c_int64)
+    cdef np.ndarray[long, ndim=3, mode='c'] cpp_segmentation = np.ascontiguousarray(segmentation, dtype=ctypes.c_int64)
     cdef np.ndarray[long, ndim=3, mode='c'] cpp_gold = np.ascontiguousarray(gold, dtype=ctypes.c_int64)
     max_segmentation = np.amax(segmentation) + 1
 
