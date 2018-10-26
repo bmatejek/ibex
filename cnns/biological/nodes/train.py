3eb15007884107c60a7ff28bbe89db17a40798e1
Brian Matejek <bmatejek@gmail.com>
2018-10-26 16:31:10 -0400

    train: added multi processing

--- cnns/biological/nodes/train.py
+++ cnns/biological/nodes/train.py
@@ -254,7 +254,7 @@ def Train(parameters, model_prefix, width, radius):
     # train the model
     history = model.fit_generator(NodeGenerator(parameters, width, radius, 'training'), steps_per_epoch=(examples_per_epoch / batch_size), 
         epochs=2000, verbose=1, class_weight=weights, callbacks=callbacks, validation_data=NodeGenerator(parameters, width, radius, 'validation'), 
-        validation_steps=(nvalidation_examples / batch_size), initial_epoch=starting_epoch)
+                                  validation_steps=(nvalidation_examples / batch_size), initial_epoch=starting_epoch, max_queue_size=1000, use_multiprocessing=True, workers=1)
 
     # save the fully trained model
     model.save_weights('{}.h5'.format(model_prefix))
