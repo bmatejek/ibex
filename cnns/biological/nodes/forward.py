17fb202e3cd0e6cfe3add0592ebc8330a1da5de8
bmatejek <bmatejek@gmail.com>
2018-10-29 22:08:43 -0400

    forward: merges nodes and full pipeline

--- cnns/biological/nodes/forward.py
+++ cnns/biological/nodes/forward.py
@@ -1,69 +1,248 @@
 import os
 import time
 import numpy as np
+import sys
+import struct
 
 from keras.models import model_from_json
 
 from ibex.utilities import dataIO
 from ibex.utilities.constants import *
+from ibex.transforms import seg2seg, seg2gold
 from ibex.cnns.biological.nodes.util import AugmentFeature
 from ibex.evaluation.classification import Prob2Pred, PrecisionAndRecall
+from ibex.graphs.biological.util import FindSmallSegments
+from ibex.evaluation import comparestacks
 
-def NodeGenerator(filenames, width):
-    filename_index = 0
+
+
+# generator for the inference of the neural network
+def NodeGenerator(examples, width):
+    index = 0
     start_time = time.time()
+
     while True:
-        # prevent overflow of the queue
-        if filename_index == len(filenames):
-            filename_index = 0
-
-        if (filename_index + 1) % 1000 == 0:
-            print '{}/{} in {:0.4f} seconds'.format(filename_index, len(filenames), time.time() - start_time)
+        # print out the progress
+        if index and not (index % 1000):
+            print '{}/{} in {:0.4f} seconds'.format(index, examples.shape[0], time.time() - start_time)
                                     
-        candidate = dataIO.ReadH5File(filenames[filename_index], 'main')
+        # prevent overflow of the queue (these examples will not go through)
+        if index == examples.shape[0]:
+            index = 0
 
-        # update the filename index
-        filename_index += 1
+        # augment the feature
+        example = AugmentFeature(examples[index], width)
+
+        # update the index
+        index += 1
         
-        yield AugmentFeature(candidate, width)
+        yield example
 
 
 
-def Forward(prefix, model_prefix, width, radius, subset):
+def CollectExamples(prefix, width, radius, subset):
+    # get the parent directory with all of the featuers
+    parent_directory = 'features/biological/nodes-{}nm-{}x{}x{}'.format(radius, width[IB_Z+1], width[IB_Y+1], width[IB_X+1])
+    
+    positive_filename = '{}/{}/positives/{}-examples.h5'.format(parent_directory, subset, prefix)
+    positive_examples = dataIO.ReadH5File(positive_filename, 'main')
+
+    negative_filename = '{}/{}/negatives/{}-examples.h5'.format(parent_directory, subset, prefix)
+    negative_examples = dataIO.ReadH5File(negative_filename, 'main')
+    
+    unknowns_filename = '{}/{}/unknowns/{}-examples.h5'.format(parent_directory, subset, prefix)
+    unknowns_example = dataIO.ReadH5File(unknowns_filename, 'main')
+
+    # concatenate all of the examples together
+    examples = np.concatenate((positive_examples, negative_examples, unknowns_example), axis=0)
+    #examples = np.concatenate((positive_examples, negative_examples), axis=0)
+    
+    # add in information needed for forward inference [regions masked out for training and validation]
+    forward_positive_filename = '{}/forward/positives/{}-examples.h5'.format(parent_directory, prefix)
+    if os.path.exists(forward_positive_filename):
+        forward_positive_examples = dataIO.ReadH5File(forward_positive_filename, 'main')
+        examples = np.concanate((examples, forward_positive_examples), axis=0)
+
+    forward_negative_filename = '{}/forward/negatives/{}-examples.h5'.format(parent_directory, prefix)
+    if os.path.exists(forward_negative_filename):
+        forward_negative_examples = dataIO.ReadH5File(forward_negative_filename, 'main')
+        examples = np.concanate((examples, forward_negative_examples), axis=0)
+
+    forward_unknowns_filename = '{}/forward/unknowns/{}-examples.h5'.format(parent_directory, prefix)
+    if os.path.exists(forward_unknowns_filename):
+        forward_unknowns_examples = dataIO.ReadH5File(forward_unknowns_filename, 'main')
+        examples = np.concanate((examples, forward_unknowns_examples), axis=0)
+
+    return examples, positive_examples.shape[0], negative_examples.shape[0]
+
+
+
+def CollectLargeSmallPairs(prefix, width, radius, subset):
+    # get the parent directory with all of the featuers
+    parent_directory = 'features/biological/nodes-{}nm-{}x{}x{}'.format(radius, width[IB_Z+1], width[IB_Y+1], width[IB_X+1])
+    
+    examples = []
+
+    positive_filename = '{}/{}/positives/{}.examples'.format(parent_directory, subset, prefix)
+    with open(positive_filename, 'rb') as fd:
+        nexamples, = struct.unpack('q', fd.read(8))
+        for _ in range(nexamples):
+            label_one, label_two, = struct.unpack('qq', fd.read(16))
+            examples.append((label_one, label_two))
+
+    negative_filename = '{}/{}/negatives/{}.examples'.format(parent_directory, subset, prefix)
+    with open(negative_filename, 'rb') as fd:
+        nexamples, = struct.unpack('q', fd.read(8))
+        for _ in range(nexamples):
+            label_one, label_two, = struct.unpack('qq', fd.read(16))
+            examples.append((label_one, label_two))
+
+    unknowns_filename = '{}/{}/unknowns/{}.examples'.format(parent_directory, subset, prefix)
+    with open(unknowns_filename, 'rb') as fd:
+        nexamples, = struct.unpack('q', fd.read(8))
+        for _ in range(nexamples):
+            label_one, label_two, = struct.unpack('qq', fd.read(16))
+            examples.append((label_one, label_two))
+
+
+    # add in information needed for forward inference [regions masked out for training and validation]
+    forward_positive_filename = '{}/forward/positives/{}.examples'.format(parent_directory, prefix)
+    if os.path.exists(forward_positive_filename):
+        with open(forward_positive_filename, 'rb') as fd:
+            nexamples, = struct.unpack('q', fd.read(8))
+            for _ in range(nexamples):
+                label_one, label_two, = struct.unpack('qq', fd.read(16))
+                examples.append((label_one, label_two))
+
+    forward_negative_filename = '{}/forward/negatives/{}.examples'.format(parent_directory, prefix)
+    if os.path.exists(forward_negative_filename):
+        with open(forward_negative_filename, 'rb') as fd:
+            nexamples, = struct.unpack('q', fd.read(8))
+            for _ in range(nexamples):
+                label_one, label_two, = struct.unpack('qq', fd.read(16))
+                examples.append((label_one, label_two))
+
+    forward_unknowns_filename = '{}/forward/unknowns/{}.examples'.format(parent_directory, prefix)
+    if os.path.exists(forward_unknowns_filename):
+        with open(forward_unknowns_filename, 'rb') as fd:
+            nexamples, = struct.unpack('q', fd.read(8))
+            for _ in range(nexamples):
+                label_one, label_two, = struct.unpack('qq', fd.read(16))
+                examples.append((label_one, label_two))
+
+    return examples
+
+
+
+def Forward(prefix, model_prefix, segmentation, width, radius, subset, evaluate=True):
     # read in the trained model
     model = model_from_json(open('{}.json'.format(model_prefix), 'r').read())
     model.load_weights('{}-best-loss.h5'.format(model_prefix))
 
-    # get all of the candidates for this prefix
-    positive_directory = 'features/biological/nodes-{}nm-{}x{}x{}/{}/positives'.format(radius, width[IB_Z+1], width[IB_Y+1], width[IB_X+1], subset)
-    negative_directory = 'features/biological/nodes-{}nm-{}x{}x{}/{}/negatives'.format(radius, width[IB_Z+1], width[IB_Y+1], width[IB_X+1], subset)
-    unknowns_directory = 'features/biological/nodes-{}nm-{}x{}x{}/{}/unknowns'.format(radius, width[IB_Z+1], width[IB_Y+1], width[IB_X+1], subset)
+    # get all of the examples
+    examples, npositives, nnegatives = CollectExamples(prefix, width, radius, subset)
 
-    # get the positive, negative, and unknown locations for this directory
-    positive_filenames = []
-    for filename in os.listdir(positive_directory):
-        if prefix in filename:
-            positive_filenames.append('{}/{}'.format(positive_directory, filename))
+    # get all of the large-small pairings
+    pairings = CollectLargeSmallPairs(prefix, width, radius, subset)
 
-    negative_filenames = []
-    for filename in os.listdir(negative_directory):
-        if prefix in filename:
-            negative_filenames.append('{}/{}'.format(negative_directory, filename))
+    # get the list of nodes over and under the threshold
+    small_segments, large_segments = FindSmallSegments(segmentation)
+ 
+    # get all of the probabilities 
+    probabilities = model.predict_generator(NodeGenerator(examples, width), examples.shape[0])
 
-    unknowns_filenames = []
-    for filename in os.listdir(unknowns_directory):
-        if prefix in filename:
-            unknowns_filenames.append('{}/{}'.format(unknowns_directory, filename))
+    # create the correct labels for the ground truth
+    ground_truth = np.zeros(npositives + nnegatives, dtype=np.bool)
+    for iv in range(npositives):
+        ground_truth[iv] = True
 
-    filenames = positive_filenames + negative_filenames# + unknowns_filenames
+    # get the results with labeled data
+    predictions = Prob2Pred(np.squeeze(probabilities[:npositives+nnegatives]))
 
-    probabilities = model.predict_generator(NodeGenerator(filenames, width), len(filenames), max_q_size=2000)
+    # print the confusion matrix
+    output_filename = '{}-{}-inference.txt'.format(model_prefix, prefix)
+    PrecisionAndRecall(ground_truth, predictions, output_filename)
+
+    # create a mapping 
+    small_segment_predictions = dict()
+    for small_segment in small_segments:
+        small_segment_predictions[small_segment] = set()
+
+    # go through each pairing
+    for pairing, probability in zip(pairings, probabilities):
+        label_one, label_two = pairing
+        #print '{} {}'.format(label_one, label_two)
+        # make sure that either label one or two is small and the other is large
+        assert ((label_one in small_segments) ^ (label_two in small_segments))
+
+        if label_one in small_segments:
+            small_segment = label_one
+            large_segment = label_two
+        else:
+            small_segment = label_two
+            large_segment = label_one
+            
+        small_segment_predictions[small_segment].add((large_segment, probability[0]))
+
+    # begin to map the small labels
+    max_label = np.amax(segmentation) + 1
+    mapping = [iv for iv in range(max_label)]
+
+    # look at seg2gold to see how many correct segments are merged
+    seg2gold_mapping = seg2gold.Mapping(prefix)
     
-    # get some results on the known quantities
-    nknowns = len(positive_filenames) + len(negative_filenames)
-    predictions = Prob2Pred(np.squeeze(probabilities[:nknowns]))
-    labels = np.zeros(nknowns, dtype=np.bool)
-    for iv in range(len(positive_filenames)):
-        labels[iv] = True
+    ncorrect_merges = 0
+    nincorrect_merges = 0
 
-    PrecisionAndRecall(labels, predictions)
+    # go through all of the small segments
+    for small_segment in small_segments:
+        best_probability = -1
+        best_large_segment = -1
+
+        # go through all the neighboring large segments
+        for large_segment, probability in small_segment_predictions[small_segment]:
+            if probability > best_probability:
+                best_probability = probability
+                best_large_segment = large_segment
+                
+        # get all of the best large segments
+        assert (not best_large_segment == -1)
+        mapping[small_segment] = large_segment
+
+        # don't consider undetermined locations
+        if seg2gold_mapping[small_segment] < 1 or seg2gold_mapping[best_large_segment] < 1: continue
+
+        if seg2gold_mapping[small_segment] == seg2gold_mapping[best_large_segment]: ncorrect_merges += 1
+        else: nincorrect_merges += 1
+
+    print 'Results: '
+    print '  Correctly Merged: {}'.format(ncorrect_merges)
+    print '  Incorrectly Merged: {}'.format(nincorrect_merges)
+    
+    # initiate the mapping to eliminate small segments
+    seg2seg.MapLabels(segmentation, mapping)
+
+    # reduce the labels and map again
+    mapping, _ = seg2seg.ReduceLabels(segmentation)
+    seg2seg.MapLabels(segmentation, mapping)
+    
+    # get the model name (first component is architecture and third is node-)
+    model_name = model_prefix.split('/')[1]
+    output_filename = 'rhoana/{}-reduced-nodes-{}.h5'.format(prefix, model_name)
+    dataIO.WriteH5File(segmentation, output_filename, 'main')
+
+    if evaluate:
+        gold = dataIO.ReadGoldData(prefix)
+
+        sys.stdout = open('nodes/{}-reduced-nodes-{}.txt'.format(prefix, model_name), 'w')
+
+        # run the evaluation framework
+        rand_error, vi = comparestacks.VariationOfInformation(segmentation, gold)
+
+        print 'Rand Error Full: {}'.format(rand_error[0] + rand_error[1])
+        print 'Rand Error Merge: {}'.format(rand_error[0])
+        print 'Rand Error Split: {}'.format(rand_error[1])
+
+        print 'Variation of Information Full: {}'.format(vi[0] + vi[1])
+        print 'Variation of Information Merge: {}'.format(vi[0])
+        print 'Variation of Information Split: {}'.format(vi[1])
