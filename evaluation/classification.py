e1457835d0a6688816ed4211922b6dfe56142c0d
matinraayai <raayaiardani.m@northeastern.edu>
2020-07-23 14:55:59 -0400

    Minor style corrections.

--- evaluation/classification.py
+++ evaluation/classification.py
@@ -10,8 +10,10 @@ def Prob2Pred(probabilities, threshold=0.5):
     predictions = np.zeros(nentries, dtype=np.uint8)
 
     for ie in range(nentries):
-        if probabilities[ie] > threshold: predictions[ie] = True
-        else: predictions[ie] = False
+        if probabilities[ie] > threshold:
+            predictions[ie] = True
+        else:
+            predictions[ie] = False
 
     return predictions
 
@@ -44,10 +46,14 @@ def PrecisionAndRecall(ground_truth, predictions, output_filename=None, binary=T
         if binary and not (label == 0 or label == 1): continue
 
         # increment the proper variables
-        if label and prediction: TP += 1
-        elif not label and prediction: FP += 1
-        elif label and not prediction: FN += 1
-        else: TN += 1
+        if label and prediction:
+            TP += 1
+        elif not label and prediction:
+            FP += 1
+        elif label and not prediction:
+            FN += 1
+        else:
+            TN += 1
     
     # format the output string
     output_string = 'Positive Examples: {}\n'.format(TP + FN)
@@ -60,14 +66,18 @@ def PrecisionAndRecall(ground_truth, predictions, output_filename=None, binary=T
     output_string += '| {:13s}|{:7s}{:7s}  |\n'.format('Truth', '', '')
     output_string += '|{:8s}{:5s} |{:7d}{:7d}  |\n'.format('', 'Split', FP, TN)
     output_string += '+--------------+----------------+\n'
-    if TP + FP == 0: output_string += 'Precision: NaN\n'
-    else: output_string += 'Precision: {}\n'.format(float(TP) / float(TP + FP))
-    if TP + FN == 0: output_string += 'Recall: NaN\n'
-    else: output_string += 'Recall: {}\n'.format(float(TP) / float(TP + FN))
+    if TP + FP == 0:
+        output_string += 'Precision: NaN\n'
+    else:
+        output_string += 'Precision: {}\n'.format(float(TP) / float(TP + FP))
+    if TP + FN == 0:
+        output_string += 'Recall: NaN\n'
+    else:
+        output_string += 'Recall: {}\n'.format(float(TP) / float(TP + FN))
     output_string += 'Accuracy: {}'.format(float(TP + TN) / float(TP + FP + FN + TN))
 
     # output the string to the output file and standard out
-    print output_string
-    if not output_filename == None:
+    print(output_string)
+    if output_filename is not None:
         with open(output_filename, 'w') as fd:
             fd.write(output_string)
